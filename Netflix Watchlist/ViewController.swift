3df73d84dc3803b9307274c09406c0464e3c9a5e
Vasco Meerman <vazz_m@vascos-macbook-pro.local>
2016-12-07 15:25:17 +0100

    Initial Commit

---
+++ Netflix Watchlist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Netflix Watchlist
+//
+//  Created by Vasco Meerman on 07/12/2016.
+//  Copyright © 2016 Vasco Meerman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
